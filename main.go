d06b61e013df0a5e6001040d249bc0624c92d91e
sampreeth B S <sampreethsampu15@gmail.com>
2025-12-30 19:25:21 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,1 @@
-package main
 
-import (
-	"log"
-	"net/http"
-)
-
-func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
-
-	log.Println("Server running at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
